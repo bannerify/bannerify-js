3a6d6763f41e80ebd1f264a3e138b55a203bfdf3
An <16853175+ducan-ne@users.noreply.github.com>
2024-08-28 01:58:05 +0700

    chore: Update API key parameter names in client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
-import ky, { HTTPError, TimeoutError } from 'ky'
-import { getTelemetry } from './telemetry'
-import { Modification } from './interface'
-import { ErrorResponse, Result, timeoutError } from './types'
+import ky, { HTTPError, TimeoutError } from "ky"
+import { getTelemetry } from "./telemetry"
+import type { Modification } from "./interface"
+import { type ErrorResponse, type Result, timeoutError } from "./types"
 
 interface Options {
   fetch?: typeof fetch
@@ -15,7 +15,7 @@ type CreateOptions = {
   // template?: Modification
   // @default: png
   nocache?: boolean
-  format?: 'svg' | 'png'
+  format?: "svg" | "png"
 }
 
 export function createClient(apiKey: string, opts?: Options) {
@@ -29,18 +29,18 @@ export class Bannerify {
 
   constructor(
     private readonly apiKey: string,
-    private readonly opts?: Options
+    private readonly opts?: Options,
   ) {
     const telemetry = getTelemetry()
-    this.baseUrl = opts?.baseUrl || 'https://api.bannerify.co/v1'
+    this.baseUrl = opts?.baseUrl || "https://api.bannerify.co/v1"
     this.client = ky.create({
       fetch: opts?.fetch || globalThis.fetch.bind(globalThis),
       prefixUrl: this.baseUrl,
       headers: {
         Authorization: `Bearer ${apiKey}`,
-        'X-SDK-Version': `${telemetry?.sdkVersions}`,
-        'X-Platform': telemetry?.platform,
-        'X-Runtime': telemetry?.runtime,
+        "X-SDK-Version": `${telemetry?.sdkVersions}`,
+        "X-Platform": telemetry?.platform,
+        "X-Runtime": telemetry?.runtime,
       },
       timeout: opts?.timeout ?? 10e3,
     })
@@ -51,7 +51,7 @@ export class Bannerify {
     options?: CreateOptions,
   ): Promise<Result<ArrayBuffer | string>> {
     try {
-      const res = await this.client.post('templates/createImage', {
+      const res = await this.client.post("templates/createImage", {
         json: {
           modifications: options?.modifications ?? [],
           // template: JSON.stringify(options?.template ?? {}),
@@ -60,13 +60,14 @@ export class Bannerify {
           format: options?.format as string,
         },
       })
-      if (options?.format === 'svg') {
+      if (options?.format === "svg") {
         return { result: await res.text() }
       }
       return { result: await res.arrayBuffer() }
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
     } catch (e: any) {
       if (e instanceof HTTPError) {
-        return (await e.response.json())['error'] as ErrorResponse
+        return (await e.response.json())["error"] as ErrorResponse
       }
       if (e instanceof TimeoutError) {
         return timeoutError as ErrorResponse
@@ -75,45 +76,42 @@ export class Bannerify {
     }
   }
 
-  async createPdf(
-    templateId: string,
-    options?: CreateOptions,
-  ) {
-    throw new Error('Not yet implemented')
+  async createPdf(templateId: string, options?: CreateOptions) {
+    throw new Error("Not yet implemented")
   }
 
   async createPermanentImage() {
-    throw new Error('Not yet implemented')
+    throw new Error("Not yet implemented")
   }
 
   #hashText = async (text: string) => {
     const myText = new TextEncoder().encode(text)
-    const myDigest = await crypto.subtle.digest({ name: 'SHA-256' }, myText)
+    const myDigest = await crypto.subtle.digest({ name: "SHA-256" }, myText)
     const hashArray = Array.from(new Uint8Array(myDigest))
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
     return hashHex
   }
 
-  async generateImageSignedUrl(
-    templateId: string,
-    options?: CreateOptions,
-  ) {
+  async generateImageSignedUrl(templateId: string, options?: CreateOptions) {
     const apiKeyHashed = await this.#hashText(this.apiKey)
     const searchParams = new URLSearchParams()
-    searchParams.set('apiKeyHashed', apiKeyHashed)
-    if (options?.format === 'svg') {
-      searchParams.set('format', 'svg')
+    searchParams.set("apiKeyHashed", apiKeyHashed)
+    if (options?.format === "svg") {
+      searchParams.set("format", "svg")
     }
     if (options?.modifications) {
-      searchParams.set('modifications', JSON.stringify(options?.modifications))
+      searchParams.set("modifications", JSON.stringify(options?.modifications))
     }
     if (options?.nocache) {
-      searchParams.set('nocache', 'true')
+      searchParams.set("nocache", "true")
     }
-    searchParams.set('templateId', templateId)
+    searchParams.set("templateId", templateId)
     searchParams.sort()
     // TODO update to this.opts.apiKey
-    searchParams.set('sign', await this.#hashText(searchParams.toString() + searchParams.get('apiKeyHashed')))
+    searchParams.set(
+      "sign",
+      await this.#hashText(searchParams.toString() + searchParams.get("apiKeyHashed")),
+    )
     return `${this.baseUrl}/templates/signedurl?${searchParams.toString()}`
   }
 }
